a0dd0d480e2e9fa8d7b03d9b2e334f090ba7b2c5
jaemin <sonjm3518@dsm.hs.kr>
2022-09-14 01:04:12 +0900

    📑 :: User Repository

---
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/user/domain/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.cheajib.cheajibserver.domain.user.domain.repository
+
+import com.cheajib.cheajibserver.domain.user.domain.User
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface UserRepository : CrudRepository<User, UUID> {
+}
