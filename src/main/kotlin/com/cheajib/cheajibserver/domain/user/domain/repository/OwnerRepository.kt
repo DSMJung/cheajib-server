cdcca95ef96231624a080211cfe1fbfc37c6fdb3
jaemin <sonjm3518@dsm.hs.kr>
2022-09-14 07:23:50 +0900

    ♻️ :: 어노테이션 추가

--- src/main/kotlin/com/cheajib/cheajibserver/domain/user/domain/repository/OwnerRepository.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/user/domain/repository/OwnerRepository.kt
@@ -2,6 +2,8 @@ package com.cheajib.cheajibserver.domain.user.domain.repository
 
 import com.cheajib.cheajibserver.domain.user.domain.Owner
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 import java.util.UUID
 
+@Repository
 interface OwnerRepository : CrudRepository<Owner, UUID>
