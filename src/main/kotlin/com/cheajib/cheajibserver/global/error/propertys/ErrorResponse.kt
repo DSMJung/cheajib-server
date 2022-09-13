64dcb8214b8257b65f96bffe5336a449a294a388
jaemin <sonjm3518@dsm.hs.kr>
2022-09-13 21:40:33 +0900

    📑 :: (#10) ErrorResponse

---
+++ src/main/kotlin/com/cheajib/cheajibserver/global/error/propertys/ErrorResponse.kt
@@ -0,0 +1,17 @@
+package com.cheajib.cheajibserver.global.error.propertys
+
+import com.cheajib.cheajibserver.global.error.GlobalException
+
+class ErrorResponse<T>(
+    val errorStatus: Int,
+    val errorMessage: String,
+) {
+    companion object {
+        fun of(exception: GlobalException): ErrorResponse<Unit> {
+            return ErrorResponse(
+                errorStatus = exception.errorStatus,
+                errorMessage = exception.errorMessage,
+            )
+        }
+    }
+}
