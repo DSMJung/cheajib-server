878f06ea90ec6fb3aaf7198d5e2632ecbc040d6c
jaemin <sonjm3518@dsm.hs.kr>
2022-09-13 21:40:05 +0900

    📑 :: (#10) ErrorCode

---
+++ src/main/kotlin/com/cheajib/cheajibserver/global/error/propertys/ErrorCode.kt
@@ -0,0 +1,13 @@
+package com.cheajib.cheajibserver.global.error.propertys
+
+enum class ErrorCode(
+    override val errorStatus: Int,
+    override val errorMessage: String
+) : ErrorProperty {
+
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    METHOD_ARGUMENT_NOT_VALID(400, "Method Argument Not Valid"),
+    CONSTRAINT_VIOLATION(400, "Constraint Violation"),
+    DATA_INTEGRITY_VIOLATION(400, "DataIntegrity Violation"),
+    ILLEGAL_ARGUMENT(400, "Illegal Argument")
+}
