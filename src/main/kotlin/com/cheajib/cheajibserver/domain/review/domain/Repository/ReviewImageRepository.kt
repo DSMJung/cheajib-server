a47171050caf769e01b603d4b874af164e37afc0
lyutvs <lyutvs@gmail.com>
2022-09-14 01:47:23 +0900

    📑 :: ReviewImageRepository

---
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/review/domain/Repository/ReviewImageRepository.kt
@@ -0,0 +1,9 @@
+package com.cheajib.cheajibserver.domain.review.domain.Repository
+
+import com.cheajib.cheajibserver.domain.review.domain.ReviewImage
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface ReviewImageRepository : CrudRepository<ReviewImage, UUID>
