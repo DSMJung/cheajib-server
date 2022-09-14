18651d0e792c8453bdb5b98c59cb2ed2880a2c69
lyutvs <lyutvs@gmail.com>
2022-09-15 00:44:15 +0900

    📑 :: UserErrorProperty

---
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/user/exception/properties/UserErrorProperty.kt
@@ -0,0 +1,12 @@
+package com.cheajib.cheajibserver.domain.user.exception.properties
+
+import com.cheajib.cheajibserver.global.error.properties.ErrorProperty
+
+enum class UserErrorProperty(
+    override val errorMessage: String,
+    override val errorStatus: Int
+    ) : ErrorProperty {
+
+    USER_NOT_FOUND( "User Not Found",404)
+
+}
