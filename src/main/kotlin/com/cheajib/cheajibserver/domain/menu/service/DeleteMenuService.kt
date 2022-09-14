04dd96bd7b6693aa90ce2f9a1ab1ec6d75bc0e6a
jaemin <sonjm3518@dsm.hs.kr>
2022-09-15 01:55:05 +0900

    ♻️ :: @Transactional 추가

--- src/main/kotlin/com/cheajib/cheajibserver/domain/menu/service/DeleteMenuService.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/menu/service/DeleteMenuService.kt
@@ -3,13 +3,16 @@ package com.cheajib.cheajibserver.domain.menu.service
 import com.cheajib.cheajibserver.domain.menu.domain.repository.MenuRepository
 import com.cheajib.cheajibserver.domain.menu.facade.MenuFacade
 import org.springframework.stereotype.Service
-import java.util.UUID
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
 
 @Service
 class DeleteMenuService(
     private val menuFacade: MenuFacade,
     private val menuRepository: MenuRepository
 ) {
+
+    @Transactional
     fun execute(menuId: UUID) {
         val menu = menuFacade.getMenuById(menuId)
         menuRepository.delete(menu)
