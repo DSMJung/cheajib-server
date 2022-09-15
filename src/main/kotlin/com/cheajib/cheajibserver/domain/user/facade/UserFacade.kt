d8eb116fa12285788cd7caa4f28ff5a695fd86e7
jaemin <sonjm3518@dsm.hs.kr>
2022-09-16 02:18:44 +0900

    📑 :: UserFacade

---
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/user/facade/UserFacade.kt
@@ -0,0 +1,23 @@
+package com.cheajib.cheajibserver.domain.user.facade
+
+import com.cheajib.cheajibserver.domain.user.domain.User
+import com.cheajib.cheajibserver.domain.user.domain.repository.UserRepository
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class UserFacade(
+    private val userRepository: UserRepository
+) {
+
+    fun findUserByEmail(email: String): User {
+        return userRepository.findByEmail(email) ?: throw RuntimeException() // 대현이 pr 머지후 변경
+    }
+
+    fun findCurrentUser(): User {
+        val authentication =
+            SecurityContextHolder.getContext().authentication ?: throw RuntimeException() //대현이 pr머시 후 변경
+        val email = authentication.name
+        return findUserByEmail(email)
+    }
+}
