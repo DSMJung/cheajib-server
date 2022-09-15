b1974e3204b60312ca576fdb2b2e49f5db8e756b
정대현 <eogus_2513@dsm.hs.kr>
2022-09-15 10:05:51 +0900

    :bookmark_tabs: :: FeignClientErrorDecoder

---
+++ src/main/kotlin/com/cheajib/cheajibserver/infrastructure/feign/error/FeignClientErrorDecoder.kt
@@ -0,0 +1,26 @@
+package com.cheajib.cheajibserver.infrastructure.feign.error
+
+import com.cheajib.cheajibserver.infrastructure.feign.exception.FeignBadRequestException
+import com.cheajib.cheajibserver.infrastructure.feign.exception.FeignExpiredTokenException
+import com.cheajib.cheajibserver.infrastructure.feign.exception.FeignForbiddenException
+import com.cheajib.cheajibserver.infrastructure.feign.exception.FeignUnAuthorizedException
+import feign.FeignException
+import feign.Response
+import feign.codec.ErrorDecoder
+
+class FeignClientErrorDecoder : ErrorDecoder {
+
+    override fun decode(methodKey: String, response: Response): Exception {
+        if (response.status() >= 400) {
+            println(response)
+            when (response.status()) {
+                401 -> throw FeignUnAuthorizedException.EXCEPTION
+                403 -> throw FeignForbiddenException.EXCEPTION
+                419 -> throw FeignExpiredTokenException.EXCEPTION
+                else -> throw FeignBadRequestException.EXCEPTION
+            }
+        }
+
+        return FeignException.errorStatus(methodKey, response)
+    }
+}
