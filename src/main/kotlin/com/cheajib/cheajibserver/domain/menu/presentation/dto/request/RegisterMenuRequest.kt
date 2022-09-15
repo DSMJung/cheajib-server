f27950e606249f0018090b1ea361287b23027928
jaemin <sonjm3518@dsm.hs.kr>
2022-09-16 02:19:51 +0900

    ♻️ :: data class로 변경

--- src/main/kotlin/com/cheajib/cheajibserver/domain/menu/presentation/dto/request/RegisterMenuRequest.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/menu/presentation/dto/request/RegisterMenuRequest.kt
@@ -1,22 +1,25 @@
 package com.cheajib.cheajibserver.domain.menu.presentation.dto.request
 
+import com.cheajib.cheajibserver.domain.user.domain.type.Level
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
-class RegisterMenuRequest {
+data class RegisterMenuRequest (
 
     @field: Size(max = 74)
     @field: NotBlank
-    lateinit var name: String
+    var name: String,
 
     @field: Size(max = 500001)
     @field: NotBlank
-    lateinit var price: String
+    var price: String,
 
     @field: NotBlank
-    lateinit var menuImageUrl: String
+    var menuImageUrl: String,
 
     @field: Size(max = 1000)
     @field: NotBlank
-    lateinit var description: String
-}
+    var description: String,
+
+    var level: Level
+)
