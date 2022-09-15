7980562db7ea33a3e83ed14e35daeb48732219dd
jaemin <sonjm3518@dsm.hs.kr>
2022-09-16 02:19:06 +0900

    ⚡ :: 이메일로 유저 찾기 추가

--- src/main/kotlin/com/cheajib/cheajibserver/domain/user/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/user/domain/repository/UserRepository.kt
@@ -3,7 +3,9 @@ package com.cheajib.cheajibserver.domain.user.domain.repository
 import com.cheajib.cheajibserver.domain.user.domain.User
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import java.util.UUID
+import java.util.*
 
 @Repository
-interface UserRepository : CrudRepository<User, UUID>
+interface UserRepository : CrudRepository<User, UUID> {
+    fun findByEmail(email: String): User?
+}
