30b526b6d3f9b96170a90f7e7d1f8dc501f9efeb
정대현 <eogus_2513@dsm.hs.kr>
2022-09-16 23:56:36 +0900

    :recycle: :: ktlint

--- src/main/kotlin/com/cheajib/cheajibserver/domain/owner/presentation/OwnerController.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/owner/presentation/OwnerController.kt
@@ -3,11 +3,7 @@ package com.cheajib.cheajibserver.domain.owner.presentation
 import com.cheajib.cheajibserver.domain.owner.presentation.dto.request.VerifyOwnerRequest
 import com.cheajib.cheajibserver.domain.owner.service.OwnerService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @RequestMapping("/owner")
@@ -18,7 +14,11 @@ class OwnerController(
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    fun verifyOwner(@RequestBody @Valid request: VerifyOwnerRequest) {
+    fun verifyOwner(
+        @RequestBody
+        @Valid
+        request: VerifyOwnerRequest
+    ) {
         ownerService.verifyOwner(request)
     }
 }
