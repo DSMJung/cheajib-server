a5a0950cd7685f4d799ad9b86cb25b76bbd21e03
lyutvs <lyutvs@gmail.com>
2022-09-17 19:24:22 +0900

    ♻️ :: comment

--- src/main/kotlin/com/cheajib/cheajibserver/domain/auth/domain/RefreshToken.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/auth/domain/RefreshToken.kt
@@ -1,15 +1,15 @@
 package com.cheajib.cheajibserver.domain.auth.domain
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.index.Indexed
-import javax.persistence.Id
 import javax.validation.constraints.NotBlank
 
 @RedisHash(value = "RefreshToken", timeToLive = 60 * 60 * 2)
 class RefreshToken(
 
     @Id
-    val accountId: String,
+    val email: String,
 
     @Indexed
     @NotBlank
