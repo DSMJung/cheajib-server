f6f75ea8295bc88fc218aa663deede1177752d3f
정대현 <eogus_2513@dsm.hs.kr>
2022-09-17 15:34:39 +0900

    :recycle: :: ktlint import

--- src/main/kotlin/com/cheajib/cheajibserver/domain/owner/presentation/OwnerController.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/owner/presentation/OwnerController.kt
@@ -3,7 +3,11 @@ package com.cheajib.cheajibserver.domain.owner.presentation
 import com.cheajib.cheajibserver.domain.owner.presentation.dto.request.VerifyOwnerRequest
 import com.cheajib.cheajibserver.domain.owner.service.OwnerService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestBody
 import javax.validation.Valid
 
 @RequestMapping("/owner")
