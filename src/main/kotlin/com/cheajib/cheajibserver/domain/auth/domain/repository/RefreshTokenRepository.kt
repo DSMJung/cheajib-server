292e6bcb54494f07e2084ffa043fcadf3b5dc25e
lyutvs <lyutvs@gmail.com>
2022-09-17 18:25:50 +0900

    📑 :: RefreshTOkenRepository

---
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/auth/domain/repository/RefreshTokenRepository.kt
@@ -0,0 +1,10 @@
+package com.cheajib.cheajibserver.domain.auth.domain.repository
+
+import com.cheajib.cheajibserver.domain.auth.domain.RefreshToken
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface RefreshTokenRepository : CrudRepository<RefreshToken, String> {
+    fun findByToken(token: String)
+}
