76558a747411399bdfdb3685e764ea29622deab2
lyutvs <lyutvs@gmail.com>
2022-09-17 22:10:30 +0900

    📑 :: SetVeganlevelService

---
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/user/service/SetVeganLevelService.kt
@@ -0,0 +1,20 @@
+package com.cheajib.cheajibserver.domain.user.service
+
+import com.cheajib.cheajibserver.domain.user.controller.dto.SetVeganLevelRequest
+import com.cheajib.cheajibserver.domain.user.domain.User
+import com.cheajib.cheajibserver.domain.user.domain.type.Level
+import com.cheajib.cheajibserver.domain.user.facade.UserFacade
+import org.springframework.stereotype.Service
+import javax.transaction.Transactional
+
+@Service
+class SetVeganLevelService(
+    private val userFacade: UserFacade
+) {
+
+    @Transactional
+    fun execute(setVeganLevelRequest: SetVeganLevelRequest) {
+        val user: User = userFacade.getCurrentUser();
+        user.setVegan(setVeganLevelRequest.level)
+    }
+}
