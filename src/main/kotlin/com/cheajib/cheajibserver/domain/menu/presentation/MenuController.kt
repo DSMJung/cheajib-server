33013285dc60eeef5dcabecc5baaa0846030d71e
정대현 <eogus_2513@dsm.hs.kr>
2022-09-17 21:26:39 +0900

    :bookmark_tabs: :: MenuController

--- src/main/kotlin/com/cheajib/cheajibserver/domain/menu/presentation/MenuController.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/menu/presentation/MenuController.kt
@@ -1,24 +1,21 @@
 package com.cheajib.cheajibserver.domain.menu.presentation
 
 import com.cheajib.cheajibserver.domain.menu.presentation.dto.request.RegisterMenuRequest
+import com.cheajib.cheajibserver.domain.menu.presentation.dto.response.MenuListResponse
 import com.cheajib.cheajibserver.domain.menu.service.DeleteMenuService
+import com.cheajib.cheajibserver.domain.menu.service.QueryMenuListService
 import com.cheajib.cheajibserver.domain.menu.service.RegisterMenuService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
-import java.util.UUID
+import org.springframework.web.bind.annotation.*
+import java.util.*
 import javax.validation.Valid
 
 @RequestMapping("/menu")
 @RestController
 class MenuController(
     private val registerMenuService: RegisterMenuService,
-    private val deleteMenuService: DeleteMenuService
+    private val deleteMenuService: DeleteMenuService,
+    private val queryMenuListService: QueryMenuListService
 ) {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{restaurant-id}")
@@ -39,5 +36,13 @@ class MenuController(
         menuId: UUID
     ) {
         deleteMenuService.execute(menuId)
+    }
+
+    @GetMapping("/{restaurant-id}")
+    fun queryMenu(
+        @PathVariable("restaurant-id")
+        restaurantId: UUID
+    ): MenuListResponse {
+        return queryMenuListService.execute(restaurantId)
     }
 }
