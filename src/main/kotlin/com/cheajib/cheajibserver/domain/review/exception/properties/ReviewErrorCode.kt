547e331a6555eec8a47475344daf9eddc6237b3d
jaemin <sonjm3518@dsm.hs.kr>
2022-09-17 17:11:08 +0900

    ⚡ :: Review Image Not Found 추가

--- src/main/kotlin/com/cheajib/cheajibserver/domain/review/exception/properties/ReviewErrorCode.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/review/exception/properties/ReviewErrorCode.kt
@@ -6,5 +6,6 @@ enum class ReviewErrorCode(
     override val errorStatus: Int,
     override val errorMessage: String
 ) : ErrorProperty {
-    REVIEW_NOT_FOUND(404, "Review Not Found")
+    REVIEW_NOT_FOUND(404, "Review Not Found"),
+    REVIEW_IMAGE_NOT_FOUND(404, "Review Image Not Found")
 }
