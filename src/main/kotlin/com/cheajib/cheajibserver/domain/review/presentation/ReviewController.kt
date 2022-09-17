09b5d7d0c5c4c55a8835cf8ba68410ea7dd09aae
jaemin <sonjm3518@dsm.hs.kr>
2022-09-17 17:11:24 +0900

    ⚡ :: 리뷰 삭제하기 추가

--- src/main/kotlin/com/cheajib/cheajibserver/domain/review/presentation/ReviewController.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/review/presentation/ReviewController.kt
@@ -1,21 +1,25 @@
 package com.cheajib.cheajibserver.domain.review.presentation
 
 import com.cheajib.cheajibserver.domain.review.presentation.dto.request.WriteReviewRequest
+import com.cheajib.cheajibserver.domain.review.service.DeleteReviewService
 import com.cheajib.cheajibserver.domain.review.service.WriteReviewService
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 import javax.validation.Valid
 
 @RequestMapping("/review")
 @RestController
 class ReviewController(
-    private val writeReviewService: WriteReviewService
+    private val writeReviewService: WriteReviewService,
+    private val deleteReviewService: DeleteReviewService
 ) {
-
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun writeReview(
@@ -24,5 +28,14 @@ class ReviewController(
         request: WriteReviewRequest
     ) {
         writeReviewService.execute(request)
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{review-id}")
+    fun deleteReview(
+        @PathVariable("review-id")
+        reviewId: UUID
+    ) {
+        deleteReviewService.execute(reviewId)
     }
 }
