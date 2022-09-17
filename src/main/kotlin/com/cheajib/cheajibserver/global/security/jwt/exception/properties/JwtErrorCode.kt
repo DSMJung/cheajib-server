de465623eed9679d16ecf312a157f2b06e5926c0
lyutvs <lyutvs@gmail.com>
2022-09-17 18:26:29 +0900

    📑 :: JwtErrorCode

---
+++ src/main/kotlin/com/cheajib/cheajibserver/global/security/jwt/exception/properties/JwtErrorCode.kt
@@ -0,0 +1,16 @@
+package com.cheajib.cheajibserver.global.security.jwt.exception.properties
+
+import com.cheajib.cheajibserver.global.error.properties.ErrorProperty
+
+enum class JwtErrorCode(
+
+    override val errorStatus: Int,
+    override val errorMessage: String
+
+) : ErrorProperty {
+
+    JWT_VALIDATE_FAIL(401, "Token Validate Failed"),
+    JWT_EXPIRED(401, "Jwt Token Expired"),
+    JWT_SIGNATURE(401, "Invalid Signature"),
+    UNEXPECTED_TOKEN(500, "Unexpected Token Exception")
+}
