a890b77003023def51389d9e5528bd9d281fa65a
정대현 <eogus_2513@dsm.hs.kr>
2022-09-17 15:40:14 +0900

    :recycle: :: ktlint

--- src/main/kotlin/com/cheajib/cheajibserver/domain/comment/presentation/CommentController.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/comment/presentation/CommentController.kt
@@ -3,8 +3,13 @@ package com.cheajib.cheajibserver.domain.comment.presentation
 import com.cheajib.cheajibserver.domain.comment.presentation.dto.request.WriteCommentReviewRequest
 import com.cheajib.cheajibserver.domain.comment.service.WriteCommentReviewService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.*
-import java.util.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import java.util.UUID
 import javax.validation.Valid
 
 @RequestMapping("/comments")
@@ -15,8 +20,11 @@ class CommentController(
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{review-id}")
     fun writeCommentReview(
-        @PathVariable("review-id") reviewId: UUID,
-        @RequestBody @Valid request: WriteCommentReviewRequest
+        @PathVariable("review-id")
+        reviewId: UUID,
+        @RequestBody
+        @Valid
+        request: WriteCommentReviewRequest
     ) {
         writeCommentReviewService.execute(reviewId, request)
     }
--- src/main/kotlin/com/cheajib/cheajibserver/domain/review/exception/ReviewErrorCode.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/review/exception/ReviewErrorCode.kt
@@ -5,6 +5,6 @@ import com.cheajib.cheajibserver.global.error.properties.ErrorProperty
 enum class ReviewErrorCode(
     override val errorStatus: Int,
     override val errorMessage: String
-): ErrorProperty {
+) : ErrorProperty {
     REVIEW_NOT_FOUND(404, "Review Not Found")
 }
