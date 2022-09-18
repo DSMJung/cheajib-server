2c33c8deabfed7c9fa0f2521af4708d19bb6e6e8
jaemin <sonjm3518@dsm.hs.kr>
2022-09-18 23:15:29 +0900

    ♻️ :: ktlint

--- src/main/kotlin/com/cheajib/cheajibserver/domain/review/presentation/ReviewController.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/review/presentation/ReviewController.kt
@@ -1,14 +1,12 @@
 package com.cheajib.cheajibserver.domain.review.presentation
 
 import com.cheajib.cheajibserver.domain.review.presentation.dto.request.WriteReviewRequest
+import com.cheajib.cheajibserver.domain.review.service.DeleteReviewService
 import com.cheajib.cheajibserver.domain.review.service.QueryMyReviewService
 import com.cheajib.cheajibserver.domain.review.service.WriteReviewService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import com.cheajib.cheajibserver.domain.review.service.DeleteReviewService
-import com.cheajib.cheajibserver.domain.review.service.WriteReviewService
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -22,7 +20,7 @@ import javax.validation.Valid
 @RestController
 class ReviewController(
     private val writeReviewService: WriteReviewService,
-    private val queryMyReviewService: QueryMyReviewService
+    private val queryMyReviewService: QueryMyReviewService,
     private val deleteReviewService: DeleteReviewService
 ) {
     @ResponseStatus(HttpStatus.CREATED)
