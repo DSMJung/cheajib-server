7782a84f4dbc2fcd40534fe61f3ce8fbe9c4e3ec
jaemin <sonjm3518@dsm.hs.kr>
2022-09-18 15:54:44 +0900

    ⚡ :: CustomReviewRepository 상속받기

--- src/main/kotlin/com/cheajib/cheajibserver/domain/review/domain/Repository/ReviewRepository.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/review/domain/Repository/ReviewRepository.kt
@@ -1,9 +1,10 @@
 package com.cheajib.cheajibserver.domain.review.domain.Repository
 
 import com.cheajib.cheajibserver.domain.review.domain.Review
+import com.cheajib.cheajibserver.domain.user.domain.User
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface ReviewRepository : CrudRepository<Review, UUID>
+interface ReviewRepository : CrudRepository<Review, UUID>, CustomReviewRepository
