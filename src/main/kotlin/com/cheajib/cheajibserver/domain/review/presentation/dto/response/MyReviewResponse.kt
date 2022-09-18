6442cd2ce86a50923d32718c41c3fa3ba8463c98
jaemin <sonjm3518@dsm.hs.kr>
2022-09-18 18:49:51 +0900

    📑 :: MyReviewResponse

---
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/review/presentation/dto/response/MyReviewResponse.kt
@@ -0,0 +1,13 @@
+package com.cheajib.cheajibserver.domain.review.presentation.dto.response
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class MyReviewResponse(
+    val restaurantName: String,
+    val restaurantId: UUID,
+    val reviewPoint: Int,
+    val content: String,
+    val createdAt: LocalDateTime,
+    val ownerCommentResponse: OwnerCommentResponse
+)
