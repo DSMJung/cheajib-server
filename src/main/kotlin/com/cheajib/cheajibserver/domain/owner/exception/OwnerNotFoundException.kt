d0a1a6d1af32e2f83211ca90c443a36fa4e16c34
정대현 <eogus_2513@dsm.hs.kr>
2022-09-18 11:47:55 +0900

    :recycle: :: code smell & ktlint

--- src/main/kotlin/com/cheajib/cheajibserver/domain/owner/exception/OwnerNotFoundException.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/owner/exception/OwnerNotFoundException.kt
@@ -2,8 +2,7 @@ package com.cheajib.cheajibserver.domain.owner.exception
 
 import com.cheajib.cheajibserver.domain.owner.exception.error.OwnerErrorCode
 import com.cheajib.cheajibserver.global.error.GlobalException
-import com.cheajib.cheajibserver.global.error.properties.GlobalErrorCode
 
-object OwnerNotFoundException: GlobalException(OwnerErrorCode.OWNER_NOT_FOUND) {
+object OwnerNotFoundException : GlobalException(OwnerErrorCode.OWNER_NOT_FOUND) {
     val EXCEPTION = OwnerNotFoundException
 }
