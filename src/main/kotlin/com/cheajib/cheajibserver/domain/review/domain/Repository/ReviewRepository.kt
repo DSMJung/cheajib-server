6b2548a571532869086d60072722f89842da31c6
lyutvs <lyutvs@gmail.com>
2022-09-19 01:30:38 +0900

    ⚡️ :: ReviewRepository

--- src/main/kotlin/com/cheajib/cheajibserver/domain/review/domain/Repository/ReviewRepository.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/review/domain/Repository/ReviewRepository.kt
@@ -1,9 +1,12 @@
 package com.cheajib.cheajibserver.domain.review.domain.Repository
 
+import com.cheajib.cheajibserver.domain.restaurant.domain.Restaurant
 import com.cheajib.cheajibserver.domain.review.domain.Review
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface ReviewRepository : CrudRepository<Review, UUID>
+interface ReviewRepository : CrudRepository<Review, UUID> {
+    fun findAllByRestaurant(restaurant: Restaurant): Review
+}
