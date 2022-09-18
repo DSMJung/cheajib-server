907bcad4c1c0524d2460057d0b51fe4cff18764b
정대현 <eogus_2513@dsm.hs.kr>
2022-09-18 11:49:29 +0900

    :recycle: :: ktlint

--- src/main/kotlin/com/cheajib/cheajibserver/domain/owner/service/ApproveOwnerService.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/owner/service/ApproveOwnerService.kt
@@ -6,12 +6,13 @@ import com.cheajib.cheajibserver.domain.owner.exception.OwnerNotFoundException
 import com.cheajib.cheajibserver.domain.owner.presentation.dto.request.ApproveOwnerRequest
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ApproveOwnerService(
     private val ownerRepository: OwnerRepository
 ) {
-
+    @Transactional
     fun execute(request: ApproveOwnerRequest) {
         val owner: Owner = ownerRepository.findByIdOrNull(request.userId) ?: throw OwnerNotFoundException.EXCEPTION
         owner.approve()
