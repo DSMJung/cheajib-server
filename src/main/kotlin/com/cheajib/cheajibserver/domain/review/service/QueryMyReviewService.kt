49dd0032c287ceaded6a7b7cf83e7860e6c91417
jaemin <sonjm3518@dsm.hs.kr>
2022-09-18 15:52:35 +0900

    📑 :: 자신이 작성한 리뷰 가져오기 서비스

---
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/review/service/QueryMyReviewService.kt
@@ -0,0 +1,23 @@
+package com.cheajib.cheajibserver.domain.review.service
+
+import com.cheajib.cheajibserver.domain.review.domain.Repository.ReviewRepository
+import com.cheajib.cheajibserver.domain.review.domain.Repository.vo.MyReviewListVO
+import com.cheajib.cheajibserver.domain.user.domain.User
+import com.cheajib.cheajibserver.domain.user.facade.UserFacade
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
+
+@Service
+class QueryMyReviewService(
+    private val userFacade: UserFacade,
+    private val reviewRepository: ReviewRepository
+) {
+    @Transactional(readOnly = true)
+    fun execute(): MyReviewListVO {
+        val user: User = userFacade.getCurrentUser()
+        val myReviewList = reviewRepository.queryMyReviewList(user)
+
+        return MyReviewListVO(myReviewList)
+    }
+}
