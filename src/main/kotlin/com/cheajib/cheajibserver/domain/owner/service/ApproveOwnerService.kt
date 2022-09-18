d675630c7b4f785ac4f983d37b95113845e2ed63
정대현 <eogus_2513@dsm.hs.kr>
2022-09-18 11:32:26 +0900

    :bookmark_tabs: :: ApproveOwnerService

---
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/owner/service/ApproveOwnerService.kt
@@ -0,0 +1,19 @@
+package com.cheajib.cheajibserver.domain.owner.service
+
+import com.cheajib.cheajibserver.domain.owner.domain.Owner
+import com.cheajib.cheajibserver.domain.owner.domain.repository.OwnerRepository
+import com.cheajib.cheajibserver.domain.owner.exception.OwnerNotFoundException
+import com.cheajib.cheajibserver.domain.owner.presentation.dto.request.ApproveOwnerRequest
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+
+@Service
+class ApproveOwnerService(
+    private val ownerRepository: OwnerRepository
+) {
+
+    fun execute(request: ApproveOwnerRequest) {
+        val owner: Owner = ownerRepository.findByIdOrNull(request.userId) ?: throw OwnerNotFoundException.EXCEPTION
+        owner.approve()
+    }
+}
