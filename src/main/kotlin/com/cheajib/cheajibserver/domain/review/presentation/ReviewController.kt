e767b8591b16805e0f5bfc2f187230e28ebf6d05
jaemin <sonjm3518@dsm.hs.kr>
2022-09-18 15:53:02 +0900

    ⚡ :: 자기가 작성한 리뷰 가져오기 Controller

--- src/main/kotlin/com/cheajib/cheajibserver/domain/review/presentation/ReviewController.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/review/presentation/ReviewController.kt
@@ -1,28 +1,40 @@
 package com.cheajib.cheajibserver.domain.review.presentation
 
 import com.cheajib.cheajibserver.domain.review.presentation.dto.request.WriteReviewRequest
+import com.cheajib.cheajibserver.domain.review.service.QueryMyReviewService
 import com.cheajib.cheajibserver.domain.review.service.WriteReviewService
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 import javax.validation.Valid
 
 @RequestMapping("/review")
 @RestController
 class ReviewController(
-    private val writeReviewService: WriteReviewService
+    private val writeReviewService: WriteReviewService,
+    private val queryMyReviewService: QueryMyReviewService
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
+    @PostMapping("/{restaurant-id}")
     fun writeReview(
+        @PathVariable("restaurant-id")
+        restaurantId: UUID,
         @Valid
         @RequestBody
         request: WriteReviewRequest
     ) {
-        writeReviewService.execute(request)
+        writeReviewService.execute(restaurantId, request)
+    }
+
+    @GetMapping
+    fun queryMyReviewList() {
+       queryMyReviewService.execute()
     }
 }
