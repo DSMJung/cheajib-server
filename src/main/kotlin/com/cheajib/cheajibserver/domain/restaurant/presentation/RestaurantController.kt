83115e9d11d58b2f823271a5c9d608e6257e67cc
jaemin <sonjm3518@dsm.hs.kr>
2022-09-20 01:18:21 +0900

    ⚡ :: 식당 리뷰 보기

--- src/main/kotlin/com/cheajib/cheajibserver/domain/restaurant/presentation/RestaurantController.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/restaurant/presentation/RestaurantController.kt
@@ -1,9 +1,11 @@
 package com.cheajib.cheajibserver.domain.restaurant.presentation
 
 import com.cheajib.cheajibserver.domain.restaurant.presentation.dto.response.QueryRestaurantResponse
+import com.cheajib.cheajibserver.domain.restaurant.presentation.dto.response.QueryRestaurantReviewResponse
 import com.cheajib.cheajibserver.domain.restaurant.presentation.dto.response.RestaurantDetailsResponse
 import com.cheajib.cheajibserver.domain.restaurant.service.QueryRestaurantDetailsService
 import com.cheajib.cheajibserver.domain.restaurant.service.QueryRestaurantPreviewService
+import com.cheajib.cheajibserver.domain.restaurant.service.QueryRestaurantReviewService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,7 +16,8 @@ import java.util.UUID
 @RestController
 class RestaurantController(
     private val queryRestaurantPreviewService: QueryRestaurantPreviewService,
-    private val queryRestaurantDetailsService: QueryRestaurantDetailsService
+    private val queryRestaurantDetailsService: QueryRestaurantDetailsService,
+    private val queryRestaurantReviewService: QueryRestaurantReviewService
 ) {
 
     @GetMapping("/{restaurant-id}")
@@ -31,5 +34,13 @@ class RestaurantController(
         restaurantId: UUID
     ): RestaurantDetailsResponse {
         return queryRestaurantDetailsService.execute(restaurantId)
+    }
+
+    @GetMapping("/reviews/{restaurant-id}")
+    fun queryRestaurantReview(
+        @PathVariable("restaurant-id")
+        restaurantId: UUID
+    ): QueryRestaurantReviewResponse {
+        return queryRestaurantReviewService.execute(restaurantId)
     }
 }
