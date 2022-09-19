19fe1ddf7e57fc4c017f6a19364ff4c29f11217b
jaemin <sonjm3518@dsm.hs.kr>
2022-09-20 01:18:45 +0900

    ⚡ :: CustomRestaurantRepository 상속

--- src/main/kotlin/com/cheajib/cheajibserver/domain/restaurant/domain/repository/RestaurantRepository.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/restaurant/domain/repository/RestaurantRepository.kt
@@ -6,4 +6,4 @@ import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface RestaurantRepository : CrudRepository<Restaurant, UUID>
+interface RestaurantRepository : CrudRepository<Restaurant, UUID>, CustomRestaurantRepository
