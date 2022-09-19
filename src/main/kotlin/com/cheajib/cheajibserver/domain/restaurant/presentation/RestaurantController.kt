0ed9974203c0c65128f67f67b9eba66ae3014363
lyutvs <lyutvs@gmail.com>
2022-09-19 16:59:13 +0900

    📑 :: RestaurantController

---
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/restaurant/presentation/RestaurantController.kt
@@ -0,0 +1,25 @@
+package com.cheajib.cheajibserver.domain.restaurant.presentation
+
+import com.cheajib.cheajibserver.domain.restaurant.presentation.dto.response.QueryRestaurantResponse
+import com.cheajib.cheajibserver.domain.restaurant.service.QueryRestaurantPreviewService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import java.util.UUID
+
+@RequestMapping("/restaurants")
+class RestaurantController(
+    private val queryRestaurantPreviewService: QueryRestaurantPreviewService
+) {
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @GetMapping("/{restaurant-id}}")
+    fun queryRestaurantPreview(
+        @PathVariable("restaurant-id")
+        restaurantId: UUID
+    ): QueryRestaurantResponse {
+        return queryRestaurantPreviewService.execute(restaurantId)
+    }
+}
