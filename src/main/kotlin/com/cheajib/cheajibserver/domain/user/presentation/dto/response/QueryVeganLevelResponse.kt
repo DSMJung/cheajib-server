0a7a979f0094122a65e9d3e9ce66470c0f56f4f8
jaemin <sonjm3518@dsm.hs.kr>
2022-09-20 19:46:05 +0900

    📑 :: QueryVeganLevelResponse

---
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/user/presentation/dto/response/QueryVeganLevelResponse.kt
@@ -0,0 +1,7 @@
+package com.cheajib.cheajibserver.domain.user.presentation.dto.response
+
+import com.cheajib.cheajibserver.domain.user.domain.type.Level
+
+data class QueryVeganLevelResponse(
+    val level: Level
+)
