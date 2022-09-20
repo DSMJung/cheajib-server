c03129d3f8f6b425a48575648cc05954fd8eb3b8
jaemin <sonjm3518@dsm.hs.kr>
2022-09-20 19:13:28 +0900

    📑 :: QueryRestaurantInfoService

---
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/restaurant/service/QueryRestaurantInfoService.kt
@@ -0,0 +1,26 @@
+package com.cheajib.cheajibserver.domain.restaurant.service
+
+import com.cheajib.cheajibserver.domain.restaurant.domain.Restaurant
+import com.cheajib.cheajibserver.domain.restaurant.domain.repository.RestaurantRepository
+import com.cheajib.cheajibserver.domain.restaurant.facade.RestaurantFacade
+import com.cheajib.cheajibserver.domain.restaurant.presentation.dto.response.QueryRestaurantInfoResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
+
+@Service
+class QueryRestaurantInfoService(
+    private val restaurantFacade: RestaurantFacade
+) {
+    @Transactional(readOnly = true)
+    fun execute(restaurantId: UUID): QueryRestaurantInfoResponse {
+        val restaurant: Restaurant = restaurantFacade.getRestaurantById(restaurantId)
+
+        return QueryRestaurantInfoResponse(
+            restaurant.phoneNumber,
+            restaurant.openHours,
+            restaurant.latitude,
+            restaurant.longitude
+        )
+    }
+}
