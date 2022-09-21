bd32602439b0230216c4c07e56daf8ab58ccf554
jaemin <sonjm3518@dsm.hs.kr>
2022-09-22 03:02:53 +0900

    ♻️ :: Double로 수정

--- src/main/kotlin/com/cheajib/cheajibserver/domain/restaurant/presentation/dto/response/QueryRestaurantResponse.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/restaurant/presentation/dto/response/QueryRestaurantResponse.kt
@@ -3,7 +3,7 @@ package com.cheajib.cheajibserver.domain.restaurant.presentation.dto.response
 data class QueryRestaurantResponse(
     val restaurantName: String,
     val address: String,
-    val starPoint: Int,
+    val starPoint: Double,
     val imageUrl: String,
     val mainMenuList: String,
     val isVerify: Boolean
--- src/main/kotlin/com/cheajib/cheajibserver/domain/restaurant/service/QueryRestaurantPreviewService.kt
+++ src/main/kotlin/com/cheajib/cheajibserver/domain/restaurant/service/QueryRestaurantPreviewService.kt
@@ -4,13 +4,11 @@ import com.cheajib.cheajibserver.domain.menu.domain.repository.MenuRepository
 import com.cheajib.cheajibserver.domain.menu.exception.MenuNotFoundException
 import com.cheajib.cheajibserver.domain.restaurant.domain.Restaurant
 import com.cheajib.cheajibserver.domain.restaurant.facade.RestaurantFacade
-import com.cheajib.cheajibserver.domain.restaurant.presentation.dto.response.MainMenuListResponse
 import com.cheajib.cheajibserver.domain.restaurant.presentation.dto.response.QueryRestaurantResponse
-import com.cheajib.cheajibserver.domain.review.domain.Review
 import com.cheajib.cheajibserver.domain.review.facade.ReviewFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.util.UUID
+import java.util.*
 
 @Service
 class QueryRestaurantPreviewService(
@@ -22,16 +20,20 @@ class QueryRestaurantPreviewService(
     @Transactional(readOnly = true)
     fun execute(restaurantId: UUID): QueryRestaurantResponse {
         val restaurant: Restaurant = restaurantFacade.getRestaurantById(restaurantId)
-        val review: Review = reviewFacade.getReviewByRestaurant(restaurant)
+        val reviewList = reviewFacade.getReviewByRestaurant(restaurant)
 
-        val starAverage: Int = review.reviewPoint / 5
+        var sum = 0
+        for (review in reviewList) {
+            sum += review.reviewPoint
+        }
+        val starPoint = (sum / reviewList.size).toDouble()
 
         val mainMenu = menuRepository.findTop1ByRestaurant(restaurant) ?: throw MenuNotFoundException.EXCEPTION
 
         return QueryRestaurantResponse(
             restaurantName = restaurant.name,
             address = restaurant.address,
-            starPoint = starAverage,
+            starPoint = starPoint,
             imageUrl = restaurant.imageUrl,
             mainMenuList = mainMenu.name,
             isVerify = restaurant.isVerify
